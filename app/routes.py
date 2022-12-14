5fa6a0f720a9a487edbbb0cfd70463645df4d8b8
Sheung Wan Wong <melodywonghxy@hotmail.com>
2022-12-14 13:02:37 -0700

    wave_01 completed

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from .model.planet import planets
 
+
+planets_bp = Blueprint("planets", __name__, url_prefix = "/planets")
+
+@planets_bp.route("",methods= ["GET"])
+def display_planets():
+    response_planets = []
+    for planet in planets:
+        response_planets.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "mass": planet.mass
+        })
+        
+    return jsonify(response_planets)
