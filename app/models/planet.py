978e24707e362b8b31a95e3189ce5d2a5a9f382e
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2022-12-20 19:31:04 -0800

    connected the app to a db. created GET /planets, GET /planets/<planet_id> and POST /planets routes. refactored the planet_id validation function.

---
+++ app/models/planet.py
@@ -0,0 +1,20 @@
+# class Planet:
+#     def __init__(self, id, name, description, mass):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.mass = mass
+
+# planets = [Planet(1, "Neptune", "thick, windy", 1.024e26 ),
+#            Planet(2, "Mars", "dusty, cold desert",6.39e23),
+#            Planet(3, "Earth", "rocky, terrestrial, full of life",5.972e24)
+#            ]
+
+from app import db
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    mass = db.Column(db.String)
+    
--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,9 @@
-from flask import Blueprint, jsonify, abort, make_response
-from .model.planet import planets
+from app import db
+from flask import Blueprint, jsonify, abort, make_response, request
+from .models.planet import Planet
 
 
 planets_bp = Blueprint("planets", __name__, url_prefix = "/planets")
-
-@planets_bp.route("",methods= ["GET"])
-def display_planets():
-    response_planets = []
-    for planet in planets:
-        response_planets.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "mass": planet.mass
-        })
-        
-    return jsonify(response_planets)
 
 # ~~~~~~ Handle planet id errors ~~~~~
 def validate_id(planet_id):
@@ -29,21 +17,60 @@ def validate_id(planet_id):
         # handling invalid planet id type
         abort(make_response({"message": f"{planet_id} is an invalid planet id"}, 400))
     
-    # collect the planet data if valid id type
-    for planet in planets:
-        if planet.id == planet_id_int:
-            return planet
+    # return planet data if id in db
+    planet = Planet.query.get(planet_id)
+
     # handle nonexistant planet id
-    abort(make_response({"message": f"{planet_id} not found"}, 404))
+    if not planet:
+        abort(make_response({"message": f"{planet_id} not found"}, 404))
+
+    return planet
+
+
+
+@planets_bp.route("",methods= ["GET"])
+def display_all_planets():
+    response_planets = []
+    planets = Planet.query.all()
+    for planet in planets:
+        response_planets.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "mass": planet.mass
+        })
+        
+    return jsonify(response_planets)
 
 
 # ~~~~~~ Single planet endpoint ~~~~~~
-@planets_bp.route("/<planet_id>",methods= ["GET"])
+@planets_bp.route("/<planet_id>",methods=["GET"])
 def display_planet(planet_id):
     valid_planet = validate_id(planet_id)
     return {
-            "id": valid_planet.id,
-            "name": valid_planet.name,
-            "description": valid_planet.description,
-            "mass": valid_planet.mass,
+        "id": valid_planet.id,
+        "name": valid_planet.name,
+        "description": valid_planet.description,
+        "mass": valid_planet.mass,
     }
+
+
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+
+    if "name" not in request_body or "description" not in request_body \
+        or "mass" not in request_body:
+        abort(make_response({"message" : \
+            "Failed to create a planet because the name and/or description \
+            and/or mass are missing"}, 400))
+
+    new_planet = Planet(
+        name=request_body["name"],
+        description=request_body["description"],
+        mass=request_body["mass"])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response({"message":"planet has been created successfully"}, 200)
