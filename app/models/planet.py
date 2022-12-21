80f74a3017ce001db47a43dbfc1fa5ebc5cf7df4
larissa <larza.ault@gmail.com>
2022-12-21 14:52:27 -0800

    Updated model - plural table name and float dtype for mass attribute

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,10 +1,3 @@
-# class Planet:
-#     def __init__(self, id, name, description, mass):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.mass = mass
-
 # planets = [Planet(1, "Neptune", "thick, windy", 1.024e26 ),
 #            Planet(2, "Mars", "dusty, cold desert",6.39e23),
 #            Planet(3, "Earth", "rocky, terrestrial, full of life",5.972e24)
@@ -16,5 +9,5 @@ class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
-    mass = db.Column(db.String)
-    
+    mass = db.Column(db.Float)
+    __tablename__ = "planets"
