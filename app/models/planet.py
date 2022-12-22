e5b67aea47e2ef3ec5771b8700dd9d2b2265674b
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2022-12-21 19:01:23 -0800

    changed tale name to planets. all columns are not allowed to be null. mass data type is float.

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,10 +1,11 @@
 from app import db
 
 class Planet(db.Model):
+    __tablename__ = 'planets'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String)
-    description = db.Column(db.String)
-    mass = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    mass = db.Column(db.Float, nullable=False)
 
 # planets = [Planet(1, "Neptune", "thick, windy", 1.024e26 ),
 #            Planet(2, "Mars", "dusty, cold desert",6.39e23),
