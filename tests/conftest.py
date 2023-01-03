3177d396816d8d6b7feecbde9de6f91b22ad292b
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2023-01-03 14:40:00 -0800

    created 3 fixtures:app, client, two_saved_planets.updated app/__init__.py to accept dev and test db. added test_get_all_planets_with_empty_db_returns_empty_list

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from app import create_app, db
+from flask.signals import request_finished
+from app.models.planet import Planet
+
+@pytest.fixture
+def app():
+    app = create_app(test_config=True)
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def two_saved_planets(app):
+    earth = Planet(name="Earth", \
+            description="rocky, terrestrial, full of life", mass=5.972e24)
+    mars = Planet(name="Mars", description="dusty, cold desert", \
+            mass=6.39e23)
+    db.session.add_all([earth, mars])
+    db.session.commit()
