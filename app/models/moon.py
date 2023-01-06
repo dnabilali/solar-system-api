67b832bde93476699b92af73621f5e9ec9f6594c
larissa <larza.ault@gmail.com>
2023-01-06 14:36:13 -0800

    added from_dict moon class method

--- app/models/moon.py
+++ app/models/moon.py
@@ -4,11 +4,29 @@ class Moon(db.Model):
     __tablename__ = 'moons'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
-    size = db.Column(db.String, nullable=False)
+    size = db.Column(db.Float, nullable=False)
     description = db.Column(db.String, nullable=False)
     discovery_date = db.Column(db.DateTime, nullable=False)
     planet = db.relationship("Planet", back_populates="moons")
     planet_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+
+    @classmethod
+    def get_all_attrs(cls):
+        """
+        Returns all existing attributes (list) in Planet class
+        """
+        return [attr for attr in dir(cls) if callable(attr)]
+
+    @classmethod
+    def from_dict(cls, dict, planet, planet_id):
+        return Moon(
+            name=dict["name"],
+            size=dict["size"],
+            description=dict["description"],
+            discovery_date=dict["discovery_date"],
+            planet=planet,
+            planet_id=planet_id,
+            )
 
     def to_dict(self):
         """
@@ -21,5 +39,5 @@ class Moon(db.Model):
             "description": self.description,
             "discovery_date": self.discovery_date,
             "planet_id": self.planet_id,
-            "planet": self.planet.name,
+            #"planet": self.planet.name,
         }
