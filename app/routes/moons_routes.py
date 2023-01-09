3efb4e031832fba0539009778cddb72a50538204
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-01-08 21:22:37 -0700

    correct typo to description

--- app/routes/moons_routes.py
+++ app/routes/moons_routes.py
@@ -1,7 +1,7 @@
 from app import db
 from flask import Blueprint, jsonify, abort, make_response, request
 from ..models.moon import Moon
-
+from app.routes.planets_routes import validate_model
 moons_bp = Blueprint("moons", __name__, url_prefix = "/moons")
 
 # ~~~~~~ Planet Routes ~~~~~
@@ -13,3 +13,10 @@ def display_all_moons():
     for moon in moons:
         response_moons.append(moon.to_dict())  
     return jsonify(response_moons)
+
+@moons_bp.route("/<moon_id>",methods=["GET"])
+def display_moon(moon_id):
+    moon = validate_model(Moon, moon_id)
+    return moon.to_dict()
+
+
